c26b7159e9d19c69e5ec1a0f3aa5e3a0fc5f84e8
SamirPhuyal-SP <samirphuyal1101@gmail.com>
2020-04-09 13:51:12 +0545

    final change

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('id', 'title', 'date_added')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_filter = ('date_added',)
     list_editable = ('date_added',)
+    summernote_fields = ('content',)
 
 
 admin.site.register(Post, PostAdmin)
